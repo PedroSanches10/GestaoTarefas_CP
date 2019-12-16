1419fd6492e95bf16599d93a976f90c8e538480c
Pedro Fiuza Sanches <32898392+PedroSanches10@users.noreply.github.com>
2019-12-16 21:32:27 +0000

    Validações (Expressões Regulares)

--- Models/Funcionario.cs
+++ Models/Funcionario.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
 
 namespace GestaoTarefas_CP.Models
 {
@@ -9,18 +12,31 @@ namespace GestaoTarefas_CP.Models
     {
         public int FuncionarioId { get; set; }
 
+        [Required(ErrorMessage ="Introduza o nome")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Introduza o número")]
+        [RegularExpression(@"\d{7}", ErrorMessage = "Número Inválido")]
+        [Display(Name = "Número")]
         public string Numero { get; set; }
 
+        [Required(ErrorMessage ="Selecione o cargo ocupado")]
         public string Cargo { get; set; }
 
+        [Required(ErrorMessage = "Introduza a morada")]
         public string Morada { get; set; }
 
+        [Required(ErrorMessage = "Introduza o E-mail")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
+        [RegularExpression(@"(2\d{8})|(9[1236]\d{7})", ErrorMessage = "Telemóvel inválido")]
+        [Display(Name = "Telemóvel")]
         public string Telemovel { get; set; }
 
+        [Required(ErrorMessage = "Introduza o NIF")]
+        [RegularExpression(@"\d{9}", ErrorMessage = "Número de Contribuinte Incorreto")]
+        [Display(Name = "Número Contribuinte")]
         public string NIF { get; set; }
 
     }
