b54f93f401647211a484aaa7adaf409e67ce3ca5
Pedro Fiuza Sanches <32898392+PedroSanches10@users.noreply.github.com>
2019-12-16 14:01:28 +0000

    CRUD Funcionário e Migração

---
+++ Models/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas_CP.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Cargo { get; set; }
+
+        public string Morada { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public string NIF { get; set; }
+
+    }
+}
