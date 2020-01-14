3ad76a3bdada390d023dd78449e4390c5183cf94
CarinaTome <44405177+CarinaTome@users.noreply.github.com>
2020-01-14 15:45:56 +0000

    Atualização das Validações no Modelo Professores

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -11,11 +11,24 @@ namespace GestaoTarefas_CP.Models
 
         public int ProfessorId { get; set; }
 
+        [Required (ErrorMessage = "Insera um nome"]
+        [StringLength (30,MinimumLength = 4)]
         public string Nome { get; set; }
 
+        [Required (ErrorMessage = "Insera um numero de telemóvel")]
+        [MaxLength(9)]
+        [RegularExpression("(9[1236][0-9]{7})")]
         public string Telemovel { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage ="Insera um Email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Insera o seu Gabinete")]
+        [MaxLength (3,ErrorMessage = "O nome do Gabinete tem de ter menos de 3 caracteres")]
         public string Gabinete { get; set; }
+        
+        [Required]
         public string Disciplina { get; set; }
     }
 }
