61636b0982a2540df1e56e5410d60646bdd90935
CarinaTome <44405177+CarinaTome@users.noreply.github.com>
2020-01-14 16:21:07 +0000

    Alterações nas validações no modelo Professor

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,26 @@ namespace GestaoTarefas_CP.Models
     public class Professor
     {
 
-        public string ProfessorId { get; set; }
+        public int ProfessorId { get; set; }
 
+        [Required(ErrorMessage = "Insera um nome")]
+        [StringLength(30, MinimumLength = 4)]
         public string Nome { get; set; }
 
-        public string Contacto { get; set; }
+        [Required(ErrorMessage = "Insera um numero de telemóvel")]
+        [MaxLength(9)]
+        [RegularExpression("(9[1236][0-9]{7})")]
+        public string Telemovel { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Insera um Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Insera o seu Gabinete")]
+        [MaxLength(3, ErrorMessage = "O nome do Gabinete tem de ter menos de 3 caracteres")]
+        public string Gabinete { get; set; }
+
+        [Required]
+        public string Disciplina { get; set; }
     }
 }
