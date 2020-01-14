ad7ca0637aaa92823bacd8fa93e7bde130004f0a
CarinaTome <44405177+CarinaTome@users.noreply.github.com>
2020-01-14 16:38:01 +0000

    Atualização do Crud

--- Controllers/ProfessorsController.cs
+++ Controllers/ProfessorsController.cs
@@ -53,7 +53,7 @@ namespace GestaoTarefas_CP.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProfessorId,Nome,Contacto")] Professor professor)
+        public async Task<IActionResult> Create([Bind("ProfessorId,Nome,Telemovel,Email,Gabinete,Disciplina")] Professor professor)
         {
             if (ModelState.IsValid)
             {
