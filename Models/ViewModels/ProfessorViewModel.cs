97ad721d3d70b07b2dcea3418a98030508d68860
CarinaTome <44405177+CarinaTome@users.noreply.github.com>
2020-01-17 22:31:07 +0000

    Criação de um ViewModel Professores

---
+++ Models/ViewModels/ProfessorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas_CP.Models.ViewModels
+{
+    public class ProfessorViewModel
+    {
+        public IEnumerable <Professor> Professores { get; set; }
+
+        public int Pagina_Atual { get; set; }
+
+        public int Total_Paginas { get; set; }
+
+        public int Primeira_Pagina { get; set; }
+        public int Ultima_Pagina { get; set; }
+        public int SearchString { get; set; }
+    }
+}
